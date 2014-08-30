885e816f53b1dc07ec16cd8c7d9079f090e33023
Piotr Wilczek <piotr.wilczek@pl.abb.com>
2014-08-30 14:34:22 +0200

    component scan based configuration

--- src/main/java/name/wilu/spring/AppConfig.java
+++ src/main/java/name/wilu/spring/AppConfig.java
@@ -1,34 +1,13 @@
 package name.wilu.spring;
 
-import name.wilu.spring.repository.BookRepository;
-import name.wilu.spring.repository.PriceRepository;
-import name.wilu.spring.repository.StubBookRepository;
-import name.wilu.spring.repository.StubPriceRepository;
-import name.wilu.spring.service.BookService;
-import name.wilu.spring.service.StubBookService;
-import org.springframework.beans.factory.annotation.Autowire;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by PLPIWIL2 on 8/30/2014.
  */
 @Configuration
+@ComponentScan({"name.wilu.spring"})
 public class AppConfig {
-
-    @Bean(name = "bookService")
-    public BookService bookService() {
-        return new StubBookService();
-    }
-
-    @Bean(name = "bookRepository")
-    public BookRepository bookRepository() {
-        return new StubBookRepository();
-    }
-
-    @Bean(name = "priceRepository")
-    public PriceRepository priceRepository() {
-        return new StubPriceRepository();
-    }
 }
 

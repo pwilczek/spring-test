b70f62db7003bf1d0f613d4fdddf0c3f427b31f4
Piotr Wilczek <piotr.wilczek@pl.abb.com>
2014-08-30 14:41:54 +0200

    prototype scoped beans

--- src/main/java/name/wilu/spring/repository/StubPriceRepository.java
+++ src/main/java/name/wilu/spring/repository/StubPriceRepository.java
@@ -1,12 +1,14 @@
 package name.wilu.spring.repository;
 
 import name.wilu.spring.model.Book;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by PLPIWIL2 on 8/30/2014.
  */
 @Repository("priceRepository")
+@Scope("prototype")
 public class StubPriceRepository implements PriceRepository {
     @Override
     public long priceFor(Book book){
--- src/main/java/name/wilu/spring/service/StubBookService.java
+++ src/main/java/name/wilu/spring/service/StubBookService.java
@@ -5,12 +5,14 @@ import name.wilu.spring.model.Book;
 import name.wilu.spring.repository.BookRepository;
 import name.wilu.spring.repository.PriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by PLPIWIL2 on 8/21/2014.
  */
 @Service("bookService")
+@Scope("prototype")
 public class StubBookService implements BookService {
 
     @Autowired private BookRepository bookRepository;
